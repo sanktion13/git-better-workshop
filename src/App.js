7c01e264809f5b4b61a80df9bb737fa284b729b3
Harry Helpful <hhelpful@gmail.com>
2019-03-31 11:34:54 -0400

    Clean up dirty code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import GovernmentFixer from './GovernmentFixer';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <GovernmentFixer size="municipal" />
-        </header>
-      </div>
-    );
-  }
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <GovernmentFixer size="municipal" />
+      </header>
+    </div>
+  );
 }
-
-export default App;
--- src/GovernmentFixer.js
+++ src/GovernmentFixer.js
@@ -1,19 +1,17 @@
 import React from 'react';
 
-class GovernmentFixer extends React.Component {
-  render() {
-    let msg;
-    if (this.props.size != 'federal') {
-      msg = "We're working on it!";
-    } else {
-      msg = "This size of problem is outside of scope for us right now, but it'd be cool if you can build a project that does this!";
-    }
-    return <div>
-      <h1>Government Fixer</h1>
-      <h2>Size of Government: {this.props.size}</h2>
-      <p>{msg}</p>
-    </div>
-  }
+function solutionFor(size) {
+  return (size === 'federal') ?
+    "This size of problem is outside of scope for us right now, but it'd be cool if you can build a project that does this!" :
+    "We're working on it!";
 }
 
-export default GovernmentFixer;
+export default function GovernmentFixer(props) {
+  return (
+    <div>
+      <h1>Government Fixer</h1>
+      <h2>Size of Government: {this.props.size}</h2>
+      <p>{solutionFor(this.props.size)}</p>
+    </div>
+  );
+}
